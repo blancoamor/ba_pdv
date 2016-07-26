5d63b8e697aae889b9d711d74e8180fb09462303
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-26 20:08:03 -0300

    Added logic to order creation

--- models.py
+++ models.py
@@ -18,3 +18,16 @@ class sale_order(models.Model):
 	@api.one
 	def create_pdv_ticket(self):
 		import pdb;pdb.set_trace()
+		# Checks if a session is open
+		session_id = self.env['pos.session'].search([('state','=','opened')])
+		if not session_id:
+			raise ValidationError('No hay sesion abierta')
+		vals_pos_order = {
+			'session_id': session_id.id,
+			'name': self.name,
+			'partner_id': self.partner_id.id,
+			'location_id': self.location_id.id,
+			'user_id': self.user_id.id,
+			'pos_reference': self.client_order_ref,	
+			}
+		pos_order = self.env['pos.order'].create(vals_pos_order)	
