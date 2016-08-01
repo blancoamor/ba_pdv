430b8f631aa9a875dc30ff206b8135ed0175c6fc
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-01 19:15:35 -0300

    Fixed bug in __openerp__

--- __openerp__.py
+++ __openerp__.py
@@ -2,10 +2,10 @@
     'name': 'Blancoamor - PDV Customization',
     'category': 'POS',
     'version': '0.1',
-    'depends': ['base','product','sale','ba_sales','point_of_sale'],
+    'depends': ['base','product','sale','ba_sales','point_of_sale','sale_stock'],
     'data': [
 	#'security/ir.model.access.csv',
-	#'security/security.xml',
+	'security/security.xml',
 	#'wizard/wizard_view.xml',
 	'pdv_view.xml',
     ],
